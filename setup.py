25717f21b57254b9f5424fc002f3e7e8c6928eb9
Akki <akki.jflatdb@gmail.com>
2025-07-12 07:46:12 +0530

    Update setup.py
    Extra

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jflatdb",
-    version="0.0.1",
+    version="0.0.3",
     author="Akki",
     author_email="akki.jflatdb@gmail.com",
     description="A lightweight JSON-based flat file database system.",
